41800bfe67002b9029b0ad3c9270560188110e96
madhuv2002 <madhuv2002@gmail.com>
2018-07-16 21:19:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,3 @@
-# class Song
-# attr_accessor :name, :artist, :genre
-
-# @@count = 0
-# @@artists = []
-# @@genres = []
-
-# def initialize(name, artist, genre)
-#   @name = name
-#   @artist = artist
-#   @genre = genre
-#   @@count += 1
-#   @@genres << genre
-#   @@artists << artist
-# end
-
-# def self.count
-#   @@count
-# end
-
-# end
 
 class Song
   attr_accessor :name, :artist, :genre
